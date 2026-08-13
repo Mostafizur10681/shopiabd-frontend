08791897212c0c7575d7311e2e5c48b1335956c8
sourav <sourav10681@gmail.com>
2026-08-13 15:55:01 +0600

    shopia to mostafiz

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -100,7 +100,7 @@ export function Footer() {
                 />
               ) : (
                 <span className="text-3xl sm:text-4xl font-black italic tracking-tighter text-[#b30047] drop-shadow-sm">
-                  S<span className="text-[#e60000]">HOPIA</span>
+                  M<span className="text-[#e60000]">OSTAFIZ</span>
                 </span>
               )}
             </Link>
